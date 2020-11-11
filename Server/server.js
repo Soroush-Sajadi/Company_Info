04612369c782f4b40d869405778d5620292af8b5
Soroush Sajadi <soorooshsadjadee@gmail.com>
2020-11-11 12:35:45 +0100

    final

--- Server/server.js
+++ Server/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 const { companiesInfo } = require('./Helper_Functions/companiesInfo')
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
     const fetchCompanies = await fetch(`https://kap-csd.herokuapp.com/api/assessment/companies`);
